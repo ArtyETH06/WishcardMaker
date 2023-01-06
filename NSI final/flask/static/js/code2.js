7fdc568401dec819df5576401b0372f0cd2c5655
Arthur RAILLON <107058122+ArtyETH06@users.noreply.github.com>
2023-01-06 23:11:09 +0100

    Add files via upload

---
+++ NSI final/flask/static/js/code2.js
@@ -0,0 +1,113 @@
+
+
+
+
+const box = document.querySelector('.container')
+
+box.addEventListener('mouseenter',
+() => {
+    confetti({
+        particleCount: 70,
+        spread: 70,
+        origin: {y: 1.3, x:1}
+    
+    
+      })
+
+      confetti({
+        particleCount: 70,
+        spread: 70,
+        origin: {y: 1.3, x:0.7}
+    
+    
+      })
+
+      confetti({
+        particleCount: 70,
+        spread: 70,
+        origin: {y: 1.3, x:0.4}
+    
+    
+      })
+
+      confetti({
+        particleCount: 70,
+        spread: 70, 
+        origin: {y: 1.3, x:0.1}
+    
+    
+      })
+
+      confetti({
+        particleCount: 70,
+        spread: 70, 
+        origin: {y: 1, x:0.05}
+    
+    
+      })
+
+      confetti({
+        particleCount: 70,
+        spread: 70, 
+        origin: {y: 0.7, x:0.05}
+    
+    
+      })
+
+      confetti({
+        particleCount: 70,
+        spread: 70, 
+        origin: {y: 0.4, x:0.05}
+    
+    
+      })
+
+      confetti({
+        particleCount: 70,
+        spread: 70, 
+        origin: {y: 0.3, x:0.05}
+    
+    
+      })
+
+      confetti({
+        particleCount: 70,
+        spread: 70, 
+        origin: {y: 0.3, x:0.4}
+    
+    
+      })
+
+      confetti({
+        particleCount: 70,
+        spread: 70, 
+        origin: {y: 0.3, x:0.7}
+    
+    
+      })
+
+      confetti({
+        particleCount: 70,
+        spread: 70, 
+        origin: {y: 0.3, x:1}
+    
+    
+      })
+    
+      confetti({
+        particleCount: 70,
+        spread: 70, 
+        origin: {y: 0.6, x:1}
+    
+    
+      })
+
+      confetti({
+        particleCount: 70,
+        spread: 70, 
+        origin: {y: 0.9, x:1}
+    
+    
+      })
+    
+})
